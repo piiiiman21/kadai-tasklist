426e22f87aa5a6206cccb141372764d3d2782fea
Piiiiman21 <piiiiman21good@gmail.com>
2020-10-25 05:51:17 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks = Task.all
+      @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -14,7 +19,7 @@ class TasksController < ApplicationController
 
   def create
       @task = Task.new(task_params)
-
+      @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に追加されました'
       redirect_to @task
@@ -23,7 +28,7 @@ class TasksController < ApplicationController
       render :new
     end
   end
-
+  
   def edit
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
+  
+  get 'tasks/create'
+  get 'tasks/destroy'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
     
   resources :tasks
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
+  resources :tasks, only: [:create, :destroy]
 end
