d0ee08b58599b70a7889f18566d6b90284b32a84
Piiiiman21 <piiiiman21good@gmail.com>
2020-10-26 09:24:44 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,6 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks = Task.order(id: :desc).page(params[:page]).per(3)
     if logged_in?
       @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -18,7 +17,6 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
       @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に追加されました'
@@ -55,6 +53,13 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   
-  get 'tasks/create'
-  get 'tasks/destroy'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  
-  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-    
+  
   resources :tasks
 
   get 'signup', to: 'users#new'
